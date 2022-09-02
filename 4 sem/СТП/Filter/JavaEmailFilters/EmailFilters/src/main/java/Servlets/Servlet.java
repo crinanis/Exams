7ea12b3681b33ca38540f9c53a7009d59437855b
ksesha <72476438+crinanis@users.noreply.github.com>
2022-09-02 12:33:27 +0300

    Add files via upload

---
+++ 4 sem/СТП/Filter/JavaEmailFilters/EmailFilters/src/main/java/Servlets/Servlet.java
@@ -0,0 +1,17 @@
+package Servlets;
+
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+import java.io.IOException;
+
+
+import java.io.IOException;
+
+@WebServlet(name = "Servlet")
+public class Servlet extends jakarta.servlet.http.HttpServlet {
+    protected void doPost(jakarta.servlet.http.HttpServletRequest request, jakarta.servlet.http.HttpServletResponse response) throws jakarta.servlet.ServletException, IOException {
+
+    }
+}
